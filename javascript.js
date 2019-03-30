c1644c804c35ceee55c4e72e8679d128b3b29a2c
Agung <indoside@live.nl>
2019-03-31 00:30:21 +0100

    Tot nu toe twee functies (nog niet af). Er zal nog aanpassingen volgen

--- javascript.js
+++ javascript.js
@@ -3,19 +3,23 @@ CSSPlugin.useSVGTransformAttr = false;
 // buttons
 
 var rotateBtn = $('#rotate');
+var disappearBtn = $('#disappear');
 
 // //  elements
 var layer = $('#Layer_1');
+var wholeCard = $('#Whole_Card');
 var head = $('#HEAD');
 var behind = $('#Behind');
 var jokerTop = $('#Joker_1');
 var jokerBottom = $('#Joker_2');
 var mouth = $('#Mouth');
 var eyes = $('#Eyes');
+var shadow = $('#Shadow');
 
 
-// main timeline Rotate
-// var tlMain = new TimelineMax({ repeat: -1, paused: true, yoyo: true });
+
+
+// Idle State
 
 var tlIdle = new TimelineMax({ repeat: -1, paused: false, yoyo: true });
 
@@ -30,41 +34,89 @@ tlIdle.to(head, 1, {
   {rotationY: -180}, "-=.5")
 .fromTo(jokerBottom, 1,
   {transformOrigin: 'center top'},
-  {rotationY: -180}, "-=.5")
+  {rotationY: -180}, "-=1")
 ;
 
 
-// timelines
-function getTimeLine(){
+// timelines functions
 
-  var tlLayer = new TimelineMax({ repeat: 0, paused: false, yoyo: false });
+function getTimeLineRotate(){
+
+  var tlLayerRotate = new TimelineMax({ repeat: 0, paused: false, yoyo: false });
 
 
-  tlLayer.to(layer, 2, {
+  tlLayerRotate
+  .set(shadow, {transformOrigin:"center bottom"})
+  .to(wholeCard, 2, {
       rotation: -360,
       y: -200,
-      scale: .5,
+      scale: .25,
       repeat: 1,
       yoyo: true,
       transformOrigin: 'center',
       ease: Back.easeIn.config(1.7)
     }, "+=.5")
+    .to(shadow, 1, {
+      scale: .5,
+      yoyo: true,
+      repeat: 1,
+      ease: Back.easeIn.config(1.7)
+    }, "-=3.5")
     .to(mouth, 1, {
         // rotation: 180,
         // attr:{}
         yoyo: true,
         repeat: 1,
-        ease: Elastic.easeOut.config(1, 0.3), y: -180
+        ease: Bounce.easeOut, y: -180
       });
 
-    return tlLayer;
+    return tlLayerRotate;
+}
+
+function getTimeLineDisappear(){
+
+  var tlLayerDisappear = new TimelineMax({ repeat: 0, paused: false, yoyo: false });
+
+
+  tlLayerDisappear
+    .fromTo(wholeCard, 1, {xPercent:0, transformOrigin: 'center bottom', ease: Power1.easeIn}, {xPercent: 50, transformOrigin: 'left bottom'})
+    .fromTo(wholeCard, 1, {rotation:0}, {rotation: -10})
+    .fromTo(wholeCard, 1, {scale: 1}, {scale: .9}, "-=1")
+    .to(wholeCard, 2, {
+      ease: Back.easeIn.config(1.7),
+      // transformOrigin: 'center bottom',
+      rotationY: 180,
+      repeat: 1,
+      yoyo: true
+    })
+    .fromTo(wholeCard, 1, {skewX: 0}, {skewX: 5}, "-=2")
+    .to(head, 1,{
+      opacity: 0,
+      ease: Power1.easeOut,
+    }, "-=2")
+    .to(head, 1,{
+      yPercent: 100,
+      ease: Power1.easeOut
+    })
+    .to(head, 1 ,{
+      yPercent: 0,
+      opacity: 100,
+      ease: Power2.easeInOut
+    })
+    .fromTo([head, jokerTop, jokerBottom], .3 ,{x:0}, {x: 800})
+    .to([head, jokerTop, jokerBottom], 1, {x:0},"+=2")
+    .to(wholeCard, 1, {skewX: 0, rotation: 0, scale: 1, xPercent:0})
+    ;
+
+    return tlLayerDisappear;
 }
 
 
 
 // Eventlisteneres
 
-rotateBtn.addEventListener('click', getTimeLine);
+rotateBtn.addEventListener('click', getTimeLineRotate);
+disappearBtn.addEventListener('click', getTimeLineDisappear);
 
 
 
